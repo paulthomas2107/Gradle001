c290fab5dde70abd9cbd461772fcf347151b52a9
Paul Thomas <paulthomas2107@gmail.com>
2021-01-13 18:21:36 +0000

    Test

---
+++ src/test/java/com/pault/demo/HelloPaulTest.java
@@ -0,0 +1,10 @@
+package com.pault.demo;
+
+import org.junit.jupiter.api.Test;
+
+public class HelloPaulTest {
+    @Test
+    public void testBollocks() {
+        HelloPaul.main(new String[]{});
+    }
+}
